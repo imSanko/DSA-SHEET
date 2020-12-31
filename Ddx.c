7dc3b808143c8d1aaf346351cef189faf1d6048b
yashss1 <yash493031@gmail.com>
2020-12-31 14:23:54 +0530

    Did Some Good Changes

--- Ddx.c
+++ Ddx.c
@@ -1,28 +1,28 @@
 #include <stdio.h>
 int main()
- {
+{
     char operator;
     double first, second;
-    printf("Enter an operator (+, -, *,): ");
+    printf("Enter an operator (+, -, *, /): ");
     scanf("%c", &operator);
     printf("Enter two type of operands: ");
     scanf("%lf %lf", &first, &second);
 
     switch (operator)
-     {
-         case '+':
+    {
+    case '+':
         printf("%.1lf + %.1lf = %.1lf", first, second, first + second);
         break;
-            case '-':
+    case '-':
         printf("%.1lf - %.1lf = %.1lf", first, second, first - second);
         break;
-            case '*':
+    case '*':
         printf("%.1lf * %.1lf = %.1lf", first, second, first * second);
         break;
-            case '/':
+    case '/':
         printf("%.1lf / %.1lf = %.1lf", first, second, first / second);
         break;
-       
+
     default:
 
         printf("Error! Operator is not correct");
