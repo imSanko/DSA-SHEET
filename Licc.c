9977ab0d3500816aed3dd869044822d545abe292
yashss1 <yash493031@gmail.com>
2020-12-31 18:22:51 +0530

    Added Fail Grade

--- Licc.c
+++ Licc.c
@@ -4,15 +4,15 @@ int main()
 {
     float marks1, marks2, marks3, marks4, marks5, average;
 
-    printf("Enter marks obtained in subject 1 :");
+    printf("Enter marks obtained in subject 1 : ");
     scanf("%f", &marks1);
-    printf("Enter marks obtained in subject 2 :");
+    printf("Enter marks obtained in subject 2 : ");
     scanf("%f", &marks2);
-    printf("Enter marks obtained in subject 3 :");
+    printf("Enter marks obtained in subject 3 : ");
     scanf("%f", &marks3);
-    printf("Enter marks obtained in subject 4 :");
+    printf("Enter marks obtained in subject 4 : ");
     scanf("%f", &marks4);
-    printf("Enter marks obtained in subject 5 :");
+    printf("Enter marks obtained in subject 5 : ");
     scanf("%f", &marks5);
 
     average = (marks1 + marks2 + marks3 + marks4 + marks5) /5 ;
@@ -20,20 +20,25 @@ int main()
  
     if (average >= 90)
     {
-        printf("Grade A");
+        printf("Grade A\n");
     }
-    else if (average >= 80)
+    else if (average >= 80) 
     {
-        printf("Grade B");
+        printf("Grade B\n");
     }
     else if (average >= 70)
     {
-        printf("Grade C");
+        printf("Grade C\n");
     }
     else if (average >= 60)
     {
-        printf("Grade D");
+        printf("Grade D\n");
     }
+    else
+    {
+        printf("FAIL\n");
+    }
+    
 
     return 0;
 }
