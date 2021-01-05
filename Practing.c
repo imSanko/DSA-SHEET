8b109c311bb2cec3454284deff8a8cb326a20362
iMSanko <saptadeep555@gmail.com>
2021-01-05 13:15:33 +0530

    Adding brackets

--- Practing.c
+++ Practing.c
@@ -1,19 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h> 
-int main() {
+int main() 
+{
    int row = 2, col = 3;
    int *arr = (int *)malloc(row * col * sizeof(int)); 
    int i, j;
-   for (i = 0; i < row; i++)
+    for (i = 0; i < row; i++)
       for (j = 0; j < col; j++)
          *(arr + i*col + j) = i + j;    
-   printf("The matrix elements are:\n");
-   for (i = 0; i < row; i++) {
+            printf("The matrix elements are:\n");
+   for (i = 0; i < row; i++) 
+{
       for (j = 0; j < col; j++) {
-         printf("%d ", *(arr + i*col + j)); 
-      }
+            printf("%d ", *(arr + i*col + j)); 
+}
       printf("\n");
-   }
+}
    free(arr); 
    return 0;
 }
